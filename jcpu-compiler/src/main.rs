4a698b5478d15cf3a96f354c31813ad67ba51d95
jason hilder <jhilder95@gmail.com>
2022-11-08 20:26:55 +0200

    Removed unneeded code.

--- jcpu-compiler/src/main.rs
+++ jcpu-compiler/src/main.rs
@@ -8,16 +8,11 @@ use crate::parser::*;
 
 fn main() {
     let file_path = std::env::args().nth(1).expect("no file given");
-    let output_path = std::env::args().nth(2);
-
     let fp = Path::new(&file_path).canonicalize();
 
     match fp {
         Ok(fp) => {
             let outpath = String::from("boot.img");
-            if let Some(output) = output_path {
-                println!("output: {}", output);
-            }
 
             let jsm = fs::read_to_string(fp).expect("failed to read file.");
             let mut parser = Parser::new(&jsm.to_string());
